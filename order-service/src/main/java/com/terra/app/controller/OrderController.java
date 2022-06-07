497be2fb6c0549a58c519a883a83dc68df230b62
Jahnabi Sharma <jahnabi.s@terrapay.com>
2022-06-07 19:02:23 +0530

    added validation in the controller layer

--- order-service/src/main/java/com/terra/app/controller/OrderController.java
+++ order-service/src/main/java/com/terra/app/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.terra.app.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +23,7 @@ public class OrderController {
 	OrderService orderService;
 
 	@PostMapping
-	public Result createStore(@RequestBody Order order) {
+	public Result createStore(@Valid @RequestBody Order order) {
 		return orderService.saveOrder(order);
 	}
 
